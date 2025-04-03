9ba36f97dea1a043e1b6e17c21d1a00b35e38489
Anna Karenina Lopes Silva <akareninals@gmail.com>
2025-04-03 17:49:02 -0300

    Aula 4

--- src/main/java/edu/ifmg/produtos/services/exceptions/CategoryService.java
+++ src/main/java/edu/ifmg/produtos/services/exceptions/CategoryService.java
@@ -1,11 +1,13 @@
-package edu.ifmg.produtos.services;
+package edu.ifmg.produtos.services.exceptions;
 
 import edu.ifmg.produtos.dtos.CategoryDTO;
 import edu.ifmg.produtos.entities.Category;
 import edu.ifmg.produtos.repository.CategoryRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Optional;
@@ -27,9 +29,30 @@ public class CategoryService {
     public CategoryDTO findById(Long id){
         Optional<Category> obj = categoryRepository.findById(id);
 
-        Category category = obj.get();
+        Category category = obj.orElseThrow(()-> new ResourceNotFound("Category not foud: " + id));
         return new CategoryDTO(category);
 
+    }
+
+    @Transactional
+    public CategoryDTO insert(CategoryDTO dto){
+        Category entity = new Category();
+        entity.setName(dto.getName());
+        entity = categoryRepository.save(entity);
+        return new CategoryDTO(entity);
+    }
+
+    @Transactional
+    public CategoryDTO update(Long id, CategoryDTO dto){
+
+        try {
+            Category entity = categoryRepository.getReferenceById(id);
+            entity.setName(dto.getName());
+            entity = categoryRepository.save(entity);
+            return new CategoryDTO(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFound("Category not foud: " + id);
+        }
     }
 
 }
