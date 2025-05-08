2a7e9c40900329a27d52ccaa00354483e0500a17
Anna Karenina Lopes Silva <akareninals@gmail.com>
2025-05-08 17:42:37 -0300

    Aula 11 - junit teste de integração

--- src/test/java/edu/ifmg/produtos/services/ProductServiceTest.java
+++ src/test/java/edu/ifmg/produtos/services/ProductServiceTest.java
@@ -22,6 +22,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -43,6 +44,7 @@ class ProductServiceTest {
         existingId = 1L;
         nonExistingId = 200L;
         Product product = Factory.createProduct();
+        product.setId(existingId);
         page = new PageImpl<>(List.of(product, product));
     }
 
@@ -83,8 +85,26 @@ class ProductServiceTest {
         Page<ProductDTO> result = productService.findAll(pagina);
 
         Assertions.assertNotNull(result);
-        //Assertions.assertEquals(1, result.getContent().getFirst().getId());
+        Assertions.assertEquals(1, result.getContent().getFirst().getId());
         verify(productRepository, times(1)).findAll(pagina);
+
+    }
+
+    @Test
+    @DisplayName("Verificando a busca de um produto por um ID existente.")
+    void findByIDShouldReturnProductWhenIdExistis() {
+
+        Product p = Factory.createProduct();
+        p.setId(existingId);
+
+        when(productRepository.findById(existingId)).thenReturn(Optional.of(p));
+
+        ProductDTO dto = productService.findById(existingId);
+
+        Assertions.assertNotNull(dto);
+        Assertions.assertEquals(existingId, dto.getId());
+        verify(productRepository, times(1))
+                .findById(existingId);
 
     }
 
